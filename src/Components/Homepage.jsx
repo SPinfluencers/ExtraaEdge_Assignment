bbad747c280c9314cf38d12e4f56186176f2cf4e
Sagar Pawar <100870856+SPinfluencers@users.noreply.github.com>
2023-04-09 16:21:52 +0530

    assignment

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,43 +1,55 @@
 import React, { useEffect, useState } from "react";
 import { SingleCard } from "./SingleCard";
-import styles from '../Components/homepage.module.css'
+import styles from "../Components/homepage.module.css";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
 export const Homepage = () => {
-    const {id} = useParams
-    const [data, setData] = useState([])
-    const [editData, setEditData] = useState([])
+  const [data, setData] = useState([]);
+  const [editData, setEditData] = useState([]);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then((res) => res.json())
-        .then((data) => setData(data))
-    }, [])
-
-    useEffect((id) => {
-      axios.get('https://jsonplaceholder.typicode.com/users/' + id)
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .catch(err => console.log(err))
-    })
+      .then((data) => setData(data));
+  }, []);
 
-    const handleRemove = (id) => {
-      var updatedList = data
-      updatedList.splice(id, 1)
-      setData([...updatedList])
+  const handleEdit = ({ id, modifiedData }) => {
+    let newData = data.map((e) => {
+      if (e.id === id) {
+        //add the modified data to element
+        e = { id, ...modifiedData };
+      }
+      return e;
+    });
+    console.log("newData", newData);
+    //
+    setData([...newData]);
+  };
 
-      // let updatedList = data.filter((item) => item.id !== id)
-      // console.log(updatedList)
-      // setData(updatedList)
-    }
-    return (
-        <>
-          <div className={styles.box}>
-            {data.map((item, id) => {
-                return <SingleCard handleRemove={handleRemove} id={id} item={item} />        
-            })}
-          </div>
-        </>
-    )
+  const handleRemove = (id) => {
+    var updatedList = data;
+    updatedList.splice(id, 1);
+    setData([...updatedList]);
 
-}
+    // let updatedList = data.filter((item) => item.id !== id)
+    // console.log(updatedList)
+    // setData(updatedList)
+  };
+  return (
+    <>
+      <div className={styles.box}>
+        {data.map((item, id) => {
+          return (
+            <SingleCard
+              handleRemove={handleRemove}
+              handleEdit={handleEdit}
+              id={id}
+              item={item}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
--- src/Components/SingleCard.jsx
+++ src/Components/SingleCard.jsx
@@ -1,71 +1,131 @@
-import React from "react";
-import styles from '../Components/singlecard.module.css'
+import React, { useEffect, useRef, useState } from "react";
+import styles from "../Components/singlecard.module.css";
 import { HeartOutlined } from "@ant-design/icons";
 import { GlobalOutlined } from "@ant-design/icons";
 import { MobileOutlined } from "@ant-design/icons";
 import { MailOutlined } from "@ant-design/icons";
-import {DeleteOutlined, EditOutlined} from '@ant-design/icons'
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 
-export const SingleCard = ({item, handleRemove, id}) => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+export const ViewDetails = ({ item, styles }) => {
+  return (
+    <div className={styles.info}>
+      <h4 key={item.id}>{item.name}</h4>
+      <div className={styles.icon}>
+        <MailOutlined />
+        <p style={{ marginTop: "-5px" }}>{item.email}</p>
+      </div>
+      <div className={styles.icon}>
+        <MobileOutlined /> <p style={{ marginTop: "-5px" }}>{item.phone}</p>
+      </div>
+      <div className={styles.icon}>
+        <GlobalOutlined /> <p style={{ marginTop: "-5px" }}>{item.website}</p>
+      </div>
+    </div>
+  );
+};
 
-    const handleDelet = () => {
-        handleRemove(id)
-    }
+const EditDetails = ({ item, handleEdit, id }) => {
+  const details = useRef(item);
+  useEffect(() => {
+    
+    return () => {
+      const modifiedData = details.current;
+      handleEdit({ id, modifiedData });
+    };
+  }, []);
 
-    return (
-        <div className={styles.container}>
-            <img src="https://fs-assets-fs.s3-us-west-2.amazonaws.com/res/img/2021/04/bWjMbx6FQXuDmF2PJbEj_dummy.png" alt="img" />
-            <div className={styles.info}>
-               <h4 key={item.id}>{item.name}</h4>
-               <div className={styles.icon}>
-                <MailOutlined /><p style={{marginTop:'-5px'}}>{item.email}</p>
-            </div>
-            <div className={styles.icon}>
-              <MobileOutlined /> <p style={{marginTop:'-5px'}}>{item.phone}</p>
-            </div>
-            <div className={styles.icon}>
-              <GlobalOutlined /> <p style={{marginTop:'-5px'}}>{item.website}</p>
-            </div>
-            <div className={styles.box_1}>
-                <HeartOutlined /> 
-                <NavLink to='/edited'><EditOutlined onClick={onOpen}/></NavLink>
-                <DeleteOutlined onClick={handleDelet}/>
-            </div>
-            </div>
-            <div>
-
-            {/* <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay /> */}
-
-            {/* <ModalContent border='1px solid red' width='40%' margin='auto' backgroundColor='white'>
-                <ModalHeader>Basic Modal</ModalHeader> */}
-                
-                {/* <ModalBody> */}
-                    {/* <form>
-                        <div>
-                            <label htmlFor="name">Name :</label>
-                            <input type="text" name="name" value={data.name} onChange={e=> setData({...data, name: e.target.value})} />
-                        </div>
-                    </form> */}
-                {/* </ModalBody> */}
-
-                {/* <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>
-                       cancle
-                    </Button>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>
-                       ok
-                    </Button>
-                </ModalFooter> */}
-                {/* </ModalContent> */}
-                 
-            {/* </Modal> */}
-
-            </div>
-
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="name">Name :-</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={item.name}
+            onChange={(e) => (details.current.name = e.target.value)}
+          />
+        </div>  
+        <br />
+        <div>
+          <label htmlFor="email">Email :- </label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={item.email}
+            onChange={(e) => (details.current.email = e.target.value)}
+          />
         </div>
-    )
-}
+        <br />
+        <div>
+          <label htmlFor="phone">Phone :-</label>
+          <input
+            type="text"
+            name="phone"
+            defaultValue={item.phone}
+            onChange={(e) => (details.current.phone = e.target.value)}
+          />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="website">Website :-</label>
+          <input
+            type="text"
+            name="website"
+            defaultValue={item.website}
+            onChange={(e) => (details.current.website = e.target.value)}
+          />
+        </div>
+        <br />
+      </form>
+    </div>
+  );
+};
+
+export const SingleCard = ({ item, handleRemove, id, handleEdit }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [edit, setEdit] = useState(false);
+
+  const handleDelet = () => {
+    handleRemove(id);
+  };
+
+  return (
+    <div className={styles.container}>
+      <img
+        src="https://fs-assets-fs.s3-us-west-2.amazonaws.com/res/img/2021/04/bWjMbx6FQXuDmF2PJbEj_dummy.png"
+        alt="img"
+      />
+      <div>
+        {edit ? (
+          <EditDetails item={item} handleEdit={handleEdit} id={id} />
+        ) : (
+          <ViewDetails item={item} styles={styles} />
+        )}
+
+        <div className={styles.box_1}>
+          <HeartOutlined />
+          <EditOutlined
+            onClick={() => {
+              setEdit(!edit);
+            }}
+          />
+          <DeleteOutlined onClick={handleDelet} />
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
